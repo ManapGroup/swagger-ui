5eacd4928f02cf1001a7bbfc978d8c54c7d3deda
Martin Endler <pokusew@seznam.cz>
2016-12-27 18:32:38 +0100

    Remove unused packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var gulp = require('gulp');
 var es = require('event-stream');
@@ -16,120 +16,120 @@ var header = require('gulp-header');
 var pkg = require('./package.json');
 var order = require('gulp-order');
 var banner = ['/**',
-  ' * <%= pkg.name %> - <%= pkg.description %>',
-  ' * @version v<%= pkg.version %>',
-  ' * @link <%= pkg.homepage %>',
-  ' * @license <%= pkg.license %>',
-  ' */',
-  ''].join('\n');
+	' * <%= pkg.name %> - <%= pkg.description %>',
+	' * @version v<%= pkg.version %>',
+	' * @link <%= pkg.homepage %>',
+	' * @license <%= pkg.license %>',
+	' */',
+	''].join('\n');
 
 /**
  * Clean ups ./dist folder
  */
-gulp.task('clean', function() {
-  return gulp
-    .src('./dist', {read: false})
-    .pipe(clean({force: true}))
-    .on('error', log);
+gulp.task('clean', function () {
+	return gulp
+		.src('./dist', { read: false })
+		.pipe(clean({ force: true }))
+		.on('error', log);
 });
 
 /**
  * Processes Handlebars templates
  */
 function templates() {
-  return gulp
-    .src(['./src/main/template/**/*'])
-    .pipe(handlebars())
-    .pipe(wrap('Handlebars.template(<%= contents %>)'))
-    .pipe(declare({
-      namespace: 'Handlebars.templates',
-      noRedeclare: true, // Avoid duplicate declarations
-    }))
-    .on('error', log);
+	return gulp
+		.src(['./src/main/template/**/*'])
+		.pipe(handlebars())
+		.pipe(wrap('Handlebars.template(<%= contents %>)'))
+		.pipe(declare({
+			namespace: 'Handlebars.templates',
+			noRedeclare: true, // Avoid duplicate declarations
+		}))
+		.on('error', log);
 }
 
 /**
  * Build a distribution
  */
-gulp.task('dist', ['clean'], function() {
+gulp.task('dist', ['clean'], function () {
 
-  return es.merge(
-      gulp.src([
-        './src/main/javascript/**/*.js',
-        './node_modules/swagger-client/browser/swagger-client.js'
-      ]),
-      templates()
-    )
-    .pipe(order(['scripts.js', 'templates.js']))
-    .pipe(concat('swagger-ui.js'))
-    .pipe(wrap('(function(){<%= contents %>}).call(this);'))
-    .pipe(header(banner, { pkg: pkg } ))
-    .pipe(gulp.dest('./dist'))
-    .pipe(uglify())
-    .on('error', log)
-    .pipe(rename({extname: '.min.js'}))
-    .on('error', log)
-    .pipe(gulp.dest('./dist'))
-    .pipe(connect.reload());
+	return es.merge(
+		gulp.src([
+			'./src/main/javascript/**/*.js',
+			'./node_modules/swagger-client/browser/swagger-client.js'
+		]),
+		templates()
+	)
+		.pipe(order(['scripts.js', 'templates.js']))
+		.pipe(concat('swagger-ui.js'))
+		.pipe(wrap('(function(){<%= contents %>}).call(this);'))
+		.pipe(header(banner, { pkg: pkg }))
+		.pipe(gulp.dest('./dist'))
+		.pipe(uglify())
+		.on('error', log)
+		.pipe(rename({ extname: '.min.js' }))
+		.on('error', log)
+		.pipe(gulp.dest('./dist'))
+		.pipe(connect.reload());
 });
 
 /**
  * Processes less files into CSS files
  */
-gulp.task('less', ['clean'], function() {
+gulp.task('less', ['clean'], function () {
 
-  return gulp
-    .src([
-      './src/main/less/screen.less',
-      './src/main/less/print.less',
-      './src/main/less/reset.less'
-    ])
-    .pipe(less())
-    .on('error', log)
-    .pipe(gulp.dest('./src/main/html/css/'))
-    .pipe(connect.reload());
+	return gulp
+		.src([
+			'./src/main/less/screen.less',
+			'./src/main/less/print.less',
+			'./src/main/less/reset.less'
+		])
+		.pipe(less())
+		.on('error', log)
+		.pipe(gulp.dest('./src/main/html/css/'))
+		.pipe(connect.reload());
 });
 
 
 /**
  * Copy lib and html folders
  */
-gulp.task('copy', ['less'], function() {
+gulp.task('copy', ['less'], function () {
 
-  // copy JavaScript files inside lib folder
-  gulp
-    .src(['./lib/**/*.{js,map}'])
-    .pipe(gulp.dest('./dist/lib'))
-    .on('error', log);
+	// copy JavaScript files inside lib folder
+	gulp
+		.src(['./lib/**/*.{js,map}'])
+		.pipe(gulp.dest('./dist/lib'))
+		.on('error', log);
 
-  // copy all files inside html folder
-  gulp
-    .src(['./src/main/html/**/*'])
-    .pipe(gulp.dest('./dist'))
-    .on('error', log);
+	// copy all files inside html folder
+	gulp
+		.src(['./src/main/html/**/*'])
+		.pipe(gulp.dest('./dist'))
+		.on('error', log);
 });
 
 /**
  * Watch for changes and recompile
  */
-gulp.task('watch', function() {
-  return watch(['./src/**/*.{js,less,handlebars}'], function() {
-    gulp.start('default');
-  });
+gulp.task('watch', function () {
+	return watch(['./src/**/*.{js,less,handlebars}'], function () {
+		gulp.start('default');
+	});
 });
 
 /**
  * Live reload web server of `dist`
  */
-gulp.task('connect', function() {
-  connect.server({
-    root: 'dist',
-    livereload: true
-  });
+gulp.task('connect', function () {
+	connect.server({
+		root: 'dist',
+		livereload: true
+	});
 });
 
 function log(error) {
-  console.error(error.toString && error.toString());
+	console.error(error.toString && error.toString());
 }
 
 
